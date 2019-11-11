1b56d1273ab5d44c46bbd89d4e15fbd191e64835
Fatih Altuntaş <altuntasfatih42@gmail.com>
2019-11-11 23:58:52 +0300

    Bug fix and update

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,12 +4,13 @@ export var disguises = 3 #How many disguises you start with
 export var disguise_duration = 5 #How long a disguise cat last in seconds
 export var disguise_slowdown = 0.25
 
-var motion = Vector2()
 enum VisionMode {DARK , NIGHTVISION}
 var current_mode 
 var disguised = false
 var vision_change_on_colldown= false 
 var velocity_multpilier = 1
+
+onready var joystick_move = $"../GUI/Joystick"
 
 func _ready():
 	Global.Player =  self
@@ -19,29 +20,13 @@ func _ready():
 	update_disguise_display()
 
 func _process(delta):
-	update_motion(delta)
-	move_and_slide(motion * velocity_multpilier)
+	move_and_slide(joystick_move.output * MAX_SPEED * velocity_multpilier)
 	if disguised:
 		$Label.rect_rotation= -rotation_degrees
 		$Label.text  = str($Timer.time_left).pad_decimals(2)
 
-func update_motion(delta):
 	look_at(get_global_mouse_position())
 	
-	if Input.is_action_pressed("ui_up") and not  Input.is_action_pressed("ui_down") :
-		motion.y = clamp((motion.y - SPEED),-MAX_SPEED,0)
-	elif Input.is_action_pressed("ui_down") and not  Input.is_action_pressed("ui_up") :
-		motion.y = clamp((motion.y + SPEED),0,MAX_SPEED)
-	else:
-		motion.y = lerp(motion.y,0,FRICTION)
-		
-		
-	if Input.is_action_pressed("ui_left") and not  Input.is_action_pressed("ui_right") :
-		motion.x = clamp((motion.x - SPEED),-MAX_SPEED,0)
-	elif Input.is_action_pressed("ui_right") and not  Input.is_action_pressed("ui_left") :
-		motion.x = clamp((motion.x + SPEED),0,MAX_SPEED)
-	else:
-		motion.x = lerp(motion.x,0,FRICTION)
 		
 func _input(event):
 	if Input.is_action_just_pressed("ui_vision_mode") && not vision_change_on_colldown:
